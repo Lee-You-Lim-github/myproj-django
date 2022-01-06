1ce911bbdc230feefad4a7f118bff6989259b442
Lee-You-Lim-github <akfl513@naver.com>
2022-01-06 21:50:25 +0900

    reivew CRUD api 구현

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from shop.models import Review
+from shop.serializers import ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
