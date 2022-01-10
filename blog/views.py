b29d57a55817bce62667a5fb8fa34c10f4995943
Lee-You-Lim-github <akfl513@naver.com>
2022-01-10 14:32:05 +0900

    DRF를 활용하여 PostViewSet 생성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from blog.models import Post
+from blog.serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
