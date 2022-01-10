b1b2f65bdf0d1fd07d6ce44fc0c435da0ac55cb1
Lee-You-Lim-github <akfl513@naver.com>
2022-01-10 14:31:06 +0900

    admin에 Post 모델 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
