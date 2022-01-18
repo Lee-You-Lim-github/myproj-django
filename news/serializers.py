ba2f9d1811c1e2da6c2774eaac422b20ae89ec41
Lee-You-Lim-github <akfl513@naver.com>
2022-01-18 14:15:31 +0900

    news.Article 모델에 author필드를 추가// article api 응답에서 author 필드를 추가

--- news/serializers.py
+++ news/serializers.py
@@ -1,5 +1,6 @@
 import re
 
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from news.models import Article
 
@@ -15,11 +16,17 @@ from news.models import Article
 #        model = Article
 #        fields = ["id", "title", "content", "photo"]
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "first_name", "last_name"]
+
 # 관리자용
 class ArticleAdminSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
     class Meta:
         model = Article
-        fields = "__all__"
+        fields = ["id", "title", "content", "photo", "author"]
 
    # def validate_title(self, title):
    #     if len(title) < 3:
@@ -27,3 +34,4 @@ class ArticleAdminSerializer(serializers.ModelSerializer):
    #     if not re.search(r"[ㄱ-힣]", title):
    #         raise serializers.ValidationError("한글을 써주세요.")
    #     return title
+
--- news/views.py
+++ news/views.py
@@ -1,18 +1,62 @@
 import json
 from django.http import HttpResponse
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from news.models import Article
 from news.serializers import  ArticleAdminSerializer
 
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 
+# list, detail, create, update, delete를 1개 ViewSet에서 지원
+# 비인증 : list, detail  --(GET) // 인증 : create, update, delete
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class= ArticleAdminSerializer
     # permission_classes = [AllowAny]   # DRF 디폴트 설정
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        # if self.request.method in ("POST", "PUT", "PATCH", "DELETE"):
+
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+# 원래 form에서 유효성 검사 시, ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+#     def article_new(request):
+#         if request.method == "POST":
+#             form = ArticleForm(request.POST, request.FILES)
+#             if form.is_valid():
+#                 article = form.save(commit=False)  --> user필드를 받지 않고 저장할 때
+#                 article.author = request.user
+#                 article.save()
+#                 return redirect(...)
+#         else:
+#             form = ArticleForm()
+
+# 저장 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    # 유효성 검사가 끝나고 나서
+    # 실제 serializer.save()를 할 때 수행되는 함수
+    def perform_create(self, serializer):
+        # serializer.save는 commit=False를 지원하지 않습니다.
+        # 대신 키워드 인자를 통한 속성 지원을 지원합니다.
+        serializer.save(author=self.request.user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    # def get_serializer_class(self):
    #     return ArticleAnomymousSerializer
