497224e6fee1a55fc96a917ef8b7a2bea85ae7b2
Lee-You-Lim-github <akfl513@naver.com>
2022-01-19 10:09:43 +0900

    회원가입 API 구현(username, password, password2)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import TokenObtainPairView, TokenRefreshView, SingupAPIView
 
 
 app_name = "accounts"
@@ -7,6 +7,7 @@ app_name = "accounts"
 urlpatterns = []
 
 urlpatterns += [
+    path('api/signup/', SingupAPIView.as_view(), name='signup'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
